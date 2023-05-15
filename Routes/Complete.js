0cd1312f453ce065309f413b3e67ffea5c0e02e9
SaiIndra2003 <suryasaiindra@gmail.com>
2023-05-15 13:36:48 +0530

    Complete List's backend Route

--- Routes/Complete.js
+++ Routes/Complete.js
@@ -1,4 +1,30 @@
-const express = require("express")
+const express = require("express");
+const List = require("../Schema/ListSchema");
+
 const Router = express.Router();
+
+
+app.get("/",function(req,res){
+    List.find({status: "Completed"},function(err,CompletedTasks){
+      if(!err){
+        res.render("completed",{Tasks: CompletedTasks});
+      }
+      else{
+        console.log(err);
+        res.redirect("/");
+      }
+    });
+})
+  
+app.delete("/", function(req, res) {
+    const checkdeListId = req.body.checkbox;
+      List.findByIdAndRemove(checkdeListId, function(err) {
+        if (!err) {
+          console.log("Removed Succesfully");
+          res.redirect("/completed");
+        }
+      });
+});
+
 
 module.exports = Router;
--- app.js
+++ app.js
@@ -18,39 +18,10 @@ app.use(methodOverride('_method'));
 app.use(express.static("public"));
 app.set('view engine', 'ejs'); 
 
-
 mongoose.connect("mongodb://localhost:27017/todoListDB");
-
-
 
 app.use("/",MainRoute);
 
-
-
-
-
-
-app.get("/completed",function(req,res){
-  Item.find({status: "Completed"},function(err,CompletedTasks){
-    if(!err){
-      res.render("completed",{Tasks: CompletedTasks});
-    }
-    else{
-      console.log(err);
-      res.redirect("/");
-    }
-  });
-})
-
-app.delete("/completed/delete", function(req, res) {
-  const checkdeItemId = req.body.checkbox;
-    Item.findByIdAndRemove(checkdeItemId, function(err) {
-      if (!err) {
-        console.log("Removed Succesfully");
-        res.redirect("/completed");
-      }
-    });
-});
 app.listen(3000, function() {
   console.log("Server started on port 3000.");
 });
