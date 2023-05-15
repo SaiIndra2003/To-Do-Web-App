ea8b47090595a460ffc4959a3b3843c21e85795a
SaiIndra2003 <suryasaiindra@gmail.com>
2023-05-15 13:39:31 +0530

    Front end changes

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
 const _ = require("lodash");
 const methodOverride = require('method-override');
 
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/todoListDB");
+
 const MainRoute = require("./Routes/Main");
-
-
 
 const app = express();
 
@@ -18,7 +18,6 @@ app.use(methodOverride('_method'));
 app.use(express.static("public"));
 app.set('view engine', 'ejs'); 
 
-mongoose.connect("mongodb://localhost:27017/todoListDB");
 
 app.use("/",MainRoute);
 
